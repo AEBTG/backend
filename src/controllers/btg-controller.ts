d52a061fc8687b8528aaea5448da6131bed265aa
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2020-12-06 12:12:34 +0200

    fixing the transaction building

--- src/controllers/btg-controller.ts
+++ src/controllers/btg-controller.ts
@@ -5,6 +5,8 @@ import { UTXO } from '../model/UTXO';
 import { raw } from 'express';
 import { btgOptions, masterKey, network, getAddress } from '../config/network';
 
+const OPS = bitcoin.script.OPS;
+
 export function sendATransaction(recepient: string, path: string, utxoData: [UTXO]) {
   console.log('reached send a transaction');
   const transactionBuilder = new bitcoin.TransactionBuilder(bitcoin.networks.bitcoingoldtestnet);
@@ -13,6 +15,9 @@ export function sendATransaction(recepient: string, path: string, utxoData: [UTX
   derivedNode.network = bitcoin.networks.bitcoingoldtestnet;
 
   const key = bitcoin.ECPair.fromWIF(derivedNode.toWIF(), bitcoin.networks.bitcoingoldtestnet);
+  const hash160 = bitcoin.crypto.hash160(key.publicKey);
+
+  const spk = bitcoin.script.compile([OPS.OP_DUP, OPS.OP_HASH160, hash160, OPS.OP_EQUALVERIFY, OPS.OP_CHECKSIG]);
 
   transactionBuilder.enableBitcoinGold(true);
   transactionBuilder.setVersion(2);
@@ -22,13 +27,13 @@ export function sendATransaction(recepient: string, path: string, utxoData: [UTX
   utxoData.forEach(aUTXO => {
     amount += parseInt(aUTXO.value);
     console.log(parseInt(aUTXO.value));
-    transactionBuilder.addInput(aUTXO.txid, aUTXO.vout);
+    transactionBuilder.addInput(aUTXO.txid, aUTXO.vout, bitcoin.Transaction.DEFAULT_SEQUENCE, spk);
     currentIndex++;
   });
 
   //   const fee = transactionBuilder.build().virtualSize() * 2;
 
-  transactionBuilder.addOutput(recepient, amount - 2000);
+  transactionBuilder.addOutput(recepient, amount - currentIndex * 200);
   console.log('reached adding output');
 
   for (let i = 0; i < currentIndex; i++) {
@@ -37,7 +42,7 @@ export function sendATransaction(recepient: string, path: string, utxoData: [UTX
       key,
       undefined,
       bitcoin.Transaction.SIGHASH_ALL | bitcoin.Transaction.SIGHASH_FORKID,
-      amount
+      parseInt(utxoData[i].value)
     );
   }
 
